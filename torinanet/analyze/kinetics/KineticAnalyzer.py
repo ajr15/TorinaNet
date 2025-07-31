vectorize kinetic solver and add jacobian + molrank fix

torinanet/analyze/kinetics/KineticAnalyzer.py
@@ -3,8 +3,9 @@
 # calculates the rates of the reactions with time
 # finds concentrations at steady state (if exist) or maximal rates / concentrations
 import numpy as np
-from scipy.integrate import ode
+from scipy.integrate import ode, solve_ivp
 from typing import List, Optional
+import pandas as pd
 from ...core.RxnGraph import RxnGraph
 from ...core import Reaction, Specie
 
@@ -36,7 +37,7 @@ class KineticAnalyzer:
 
     def get_concentration(self, specie: Specie, step: Optional[int]=None) -> float:
         """Get specie concentration at a simulation step"""
-        if self._concs:
+        if self._concs is not None:
             # securing case with too large step number
             if self.rxn_graph.has_specie(specie):
                 if step is None:
@@ -55,6 +56,25 @@ class KineticAnalyzer:
                 raise ValueError("Supplied specie is not in reaction graph")
         else:
             raise RuntimeError("No simulation was ran! before reading concentrations you must call the solve_kinetics method at least once")
+
+    def concentrations_df(self) -> pd.DataFrame:
+        """Get all specie concentrations as a dataframe"""
+        columns = ["A" for _ in range(len(self._specie_d))]
+        for sp in self.rxn_graph.species:
+            sid = self.rxn_graph.specie_collection.get_key(sp)
+            idx = self._specie_d[sid]
+            columns[idx] = sp.identifier
+        return pd.DataFrame(data=self._concs, columns=columns, index=self._ts)
+    
+    def rates_df(self) -> pd.DataFrame:
+        df = self.concentrations_df()
+        ajr = {}
+        for rxn in self.rxn_graph.reactions:
+            reactants = [s.identifier for s in rxn.reactants]
+            rid = self.rxn_graph.reaction_collection.get_key(rxn)
+            rates = df[reactants].product(axis=1) * rxn.properties[self.rate_constant_property]
+            ajr[rid] = rates
+        return pd.DataFrame(ajr, index=df.index)
 
     @staticmethod
     def _build_specie_idx_dict(rxn_graph: RxnGraph) -> dict:
@@ -91,7 +111,72 @@ class KineticAnalyzer:
         # returning the total function
         return lambda t, concs: np.sum([rfunc(t, concs, k, ridxs, pidxs) for rfunc, k, ridxs, pidxs in zip(rfuncs, ks, rids, pids)], axis=0)
 
-    def solve_kinetics(self, simulation_time: float, timestep: float, initial_concs: List[float], **solver_kwargs):
+    # def solve_kinetics(self, simulation_time: float, timestep: float, initial_concs: List[float], verbose=0, **solver_kwargs):
+    #     """Solve the rate equations at given conditions.
+    #     ARGS:
+    #         - simulation_time (float): total simulation time
+    #         - timestep (float): time of each simulation step
+    #         - initial_concs (List[float]): list of initial specie concentrations
+    #         - **solver_kwargs: keywords for scipy.integrate.ode.set_integrator method
+    #     RETURNS:
+    #         None"""
+    #     # building target function
+    #     target_f = self._build_f()
+    #     # setting up solver
+    #     solver = ode(target_f)
+    #     solver.set_integrator(**solver_kwargs)
+    #     solver.set_initial_value(y=initial_concs)
+    #     # solving the ODE
+    #     t = 0
+    #     self._concs = [initial_concs]
+    #     self._ts = [0]
+    #     while solver.successful() and t < simulation_time:
+    #         if verbose > 0:
+    #             print("time =", t)
+    #         t = t + timestep
+    #         self._ts.append(t)
+    #         self._concs.append(solver.integrate(t))
+    #     if verbose > 0:
+    #         print("DONE")
+
+    # def solve_kinetics_ivp(self, initial_concs: List[float], ss_threshold: float=1e-10, max_t: Optional[float]=None, atol: float=1e-10, rtol: float=1e-3, method: str="BDF"):
+    #     """Solve the rate equations at given conditions.
+    #     ARGS:
+    #         - simulation_time (float): total simulation time
+    #         - timestep (float): time of each simulation step
+    #         - initial_concs (List[float]): list of initial specie concentrations
+    #         - **solver_kwargs: keywords for scipy.integrate.ode.set_integrator method
+    #     RETURNS:
+    #         None"""
+    #     # building target function
+    #     target_f = self._build_f()
+    #     ss_event = lambda t, y: 0 if np.abs(np.max(target_f(t, y))) < ss_threshold else 1
+    #     ss_event.terminal = True
+    #     if max_t is None:
+    #         max_t = np.max([1 / rxn.properties[self.rate_constant_property] for rxn in self.rxn_graph.reactions])
+    #     sol = solve_ivp(fun=target_f, t_span=(0, max_t), y0=initial_concs, method=method, events=ss_event, atol=atol, rtol=rtol)
+    #     self._ts = sol.t
+    #     self._concs = sol.y.T
+
+    @staticmethod
+    def jacobian(t, concs, M):
+        # ajr = np.concatenate([[1], concs.flatten("F")]).reshape(-1, 1)
+        ajr = concs.flatten("F").reshape(-1, 1)
+        n = len(ajr)
+        I = np.eye(n)
+        kron1 = np.kron(I, ajr)
+        kron2 = np.kron(ajr, I)
+        return M @ (kron1 + kron2)
+    
+    @staticmethod
+    def _target_function(t, concs, M):
+        # ajr = np.concatenate([[1], concs.flatten("F")]).reshape(-1, 1)
+        ajr = concs.flatten("F").reshape(-1, 1)
+        print(M.shape, ajr.shape, np.kron(ajr, ajr).reshape(-1, 1).shape)
+        return (M @ np.kron(ajr, ajr).reshape(-1, 1)).flatten("F")
+
+
+    def solve_kinetics(self, initial_concs: List[float], ss_threshold: float=1e-10, max_t: Optional[float]=None, atol: float=1e-10, rtol: float=1e-3, method: str="BDF"):
         """Solve the rate equations at given conditions.
         ARGS:
             - simulation_time (float): total simulation time
@@ -101,21 +186,17 @@ class KineticAnalyzer:
         RETURNS:
             None"""
         # building target function
-        target_f = self._build_f()
-        # setting up solver
-        solver = ode(target_f)
-        solver.set_integrator(**solver_kwargs)
-        solver.set_initial_value(y=initial_concs)
-        # solving the ODE
-        t = 0
-        self._concs = [initial_concs]
-        self._ts = [0]
-        while solver.successful() and t < simulation_time:
-            print("time =", t)
-            t = t + timestep
-            self._ts.append(t)
-            self._concs.append(solver.integrate(t))
-        print("DONE")
+        rate_mat = self._rate_matrix()
+        target_f = lambda t, concs: self._target_function(t, concs, rate_mat)
+        jac = lambda t, concs: self.jacobian(t, concs, rate_mat)
+        ss_event = lambda t, y: 0 if np.abs(np.max(target_f(t, y))) < ss_threshold else 1
+        ss_event.terminal = True
+        if max_t is None:
+            max_t = np.max([1 / rxn.properties[self.rate_constant_property_name] for rxn in self.g.reactions()])
+        iconcs = np.array([1] + initial_concs)
+        sol = solve_ivp(fun=target_f, jac=jac, t_span=(0, max_t), y0=iconcs, method=method, events=ss_event, atol=atol, rtol=rtol)
+        self._ts = sol.t
+        self._concs = sol.y.T[:, 1:]
 
     def find_max_reaction_rates(self, simulation_time: float, timestep: float, initial_concs: List[float], **solver_kwargs):
         """Solve the rate equations at given conditions and follow only the maximal reaction rates.
@@ -152,7 +233,7 @@ class KineticAnalyzer:
 
     def get_n_steps(self) -> int:
         """Get the total number of simulation steps. If no simulation was ran, returns None"""
-        if self._concs:
+        if self._concs is not None:
             return len(self._concs)
 
     def get_specie_index(self, sid: str):

torinanet/analyze/network_reduction/KineticReduction.py
@@ -103,7 +103,7 @@ class MolRankReduction:
         # initializing - converting to networkx graph 
         g = rxn_graph.to_networkx_graph(use_internal_id=True)
         # initializing the MolRank dictionary with values for each specie in graph
-        ajr = pd.DataFrame({"p": np.zeros(rxn_graph.get_n_species()), "visited": np.zeros(rxn_graph.get_n_species()), "specie": rxn_graph.species}, index=list(rxn_graph.specie_collection.keys()))
+        ajr = pd.DataFrame({"p": np.zeros(rxn_graph.get_n_species()), "visited": [False for _ in range(rxn_graph.get_n_species())], "specie": rxn_graph.species}, index=list(rxn_graph.specie_collection.keys()))
         # setting seed probabilities of 1 for all source species
         seed = [rxn_graph.specie_collection.get_key(sp) for sp  in rxn_graph.source_species]
         for key in seed:
@@ -149,7 +149,8 @@ class MolRankReduction:
     def rank_reactions(self, rxn_graph: RxnGraph):
         # get the maximal distance from source as number of iterations for MolRank
         analyzer = ShortestPathAnalyzer(rxn_graph, prop_func=lambda rxn: 1)
-        n_iterations = int(max(analyzer.shortest_path_table["dist"].values))
+        ajr = analyzer.shortest_path_table
+        n_iterations = int(max(ajr[ajr["dist"] < np.inf]["dist"].values))
         # initializing - converting to networkx graph 
         g = rxn_graph.to_networkx_graph(use_internal_id=True)
         # initializing rank dataframe